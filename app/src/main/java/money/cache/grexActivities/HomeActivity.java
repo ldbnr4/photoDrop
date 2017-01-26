5155665c91b380d03bdff0dbc0cc89dc8f45e625
Lorenzo Boyice <lorenzo.boyice@garmin.com>
2017-01-26 15:56:55 -0600

    Filled list view

--- app/src/main/java/money/cache/grexActivities/HomeActivity.java
+++ app/src/main/java/money/cache/grexActivities/HomeActivity.java
@@ -4,12 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import grexClasses.GrexSocket;
+import grexClasses.Room;
 import grexClasses.User;
 
 //TODO: make a splash screen for whatever page is the home page like 'splash screens -> down ken burns' from UI app
@@ -21,7 +25,7 @@ public class HomeActivity extends AppCompatActivity {
     @Bind(R.id.btn_createRoom)
     Button _createRoomBtn;
 
-    //TODO: populate list view eg: https://github.com/ldbnr4/DonateTheDistance/blob/master/app/src/main/java/com/example/lorenzo/donatethedistance/ProfilePageView.java
+    //TODO: convert listview to cards
     @Bind(R.id.list_listOfUserRooms)
     ListView _listView;
 
@@ -45,7 +49,12 @@ public class HomeActivity extends AppCompatActivity {
         GrexSocket.emit_getRooms();
         //TODO: add timeout element
         while(!GrexSocket.roomUpdate);
-        System.out.println(user.getRoomsIn().size());
-        System.out.println("GOT EM");
+        ArrayAdapter<Room> arrayAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                new ArrayList<>(new ArrayList<>(user.getRoomsIn()))
+        );
+        _listView.setAdapter(arrayAdapter);
+
     }
 }
