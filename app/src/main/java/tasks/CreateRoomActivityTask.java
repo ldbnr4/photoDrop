05725cf5131409d33d8a6e0e280c11176321d2ef
Lorenzo Boyice <lorenzo.boyice@garmin.com>
2017-02-01 08:59:46 -0600

    update to ignore

--- app/src/main/java/tasks/CreateRoomActivityTask.java
+++ app/src/main/java/tasks/CreateRoomActivityTask.java
@@ -1,8 +1,22 @@
 package tasks;
 
+import grexEnums.RET_STATUS;
+import money.cache.grexActivities.SocketActivity;
+
+import static grexClasses.GrexSocket.getRoomsStatus;
+
 /**
  * Created by boyice on 1/31/2017.
+ *
  */
 
-public class CreateRoomActivityTask {
+public class CreateRoomActivityTask extends SocketActivityTask {
+    CreateRoomActivityTask(SocketActivity socketActivity) {
+        super(socketActivity, getRoomsStatus);
+    }
+
+    @Override
+    protected RET_STATUS doInBackground(String... params) {
+        return null;
+    }
 }
